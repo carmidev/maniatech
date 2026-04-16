6a46b04d884d56de2b65caaba4d1194b8c2dc443
Miguel Linarez <miguelinarez626@gmail.com>
2026-04-16 00:36:41 -0400

    chore: Vercel migration config update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export",
-  basePath: isProd ? "/DolceCandy" : "",
-  assetPrefix: isProd ? "/DolceCandy/" : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/DolceCandy" : "",
-  },
   images: {
     unoptimized: true,
   },
