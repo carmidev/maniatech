e8b93285009bebc2f995c129495e7bce6e875390
caroust <miguelinarez26@users.noreply.github.com>
2026-05-12 15:22:38 -0400

    arreglanodo error subida de diseño y cambios de anak

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -7,7 +7,7 @@ export interface Candy {
   images: string[];
   category: string | string[];
   tag?: string;
-  badge?: "nuevo" | "bestseller" | "viral" | "exclusivo";
+  badge?: "nuevo" | "bestseller" | "viral" | "exclusivo" | "menu";
   stock?: number;
 }
 
